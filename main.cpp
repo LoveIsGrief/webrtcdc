4dcd99b8f624543090aa41159b4e1d0ac616412a
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2018-08-05 18:50:15 +0200

    Make sure usrsctp is built before librtcdcpp
    And compile it for the first time while including librtcdcpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "rtcdcpp/PeerConnection.hpp"
 
-using namespace std;
+int main(){
+    std::cout << "Building configuration" << std::endl;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    rtcdcpp::RTCIceServer iceServer = {
+        std::string(""),
+        0
+    };
+    auto iceServers = std::vector<rtcdcpp::RTCIceServer>();
+    iceServers.push_back(iceServer);
+    rtcdcpp::RTCConfiguration config = {
+        iceServers
+    };
+
     return 0;
 }
 
